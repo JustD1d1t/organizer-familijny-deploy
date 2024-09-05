11c4b41152877711235e88f95cd1a927c72ec3fe
JustD1d1t <starzyk.dawid.0826@gmail.com>
2024-09-05 12:22:18 +0200

    deploy

--- assets/ceq9LQWH.js
+++
@@ -1,3 +0,0 @@
-import{be as i,bf as a,bg as c,bh as d,bi as l}from"./Chhd-YqF.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const h=()=>{const e=window;e.addEventListener("statusTap",()=>{i(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=a(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{h as startStatusTap};
