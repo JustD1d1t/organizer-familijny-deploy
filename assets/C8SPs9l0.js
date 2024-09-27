203b76f3888ec188c44e088f914066137298c1fd
JustD1d1t <starzyk.dawid.0826@gmail.com>
2024-09-27 16:00:16 +0200

    deploy

--- assets/C8SPs9l0.js
+++
@@ -1,3 +0,0 @@
-import{bf as i,bg as a,bh as c,bi as d,bj as l}from"./BVzA7Ocv.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const h=()=>{const e=window;e.addEventListener("statusTap",()=>{i(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=a(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{h as startStatusTap};
