dc9efa25765d5c69b469e4ef20bc3a806c95557f
JustD1d1t <starzyk.dawid.0826@gmail.com>
2024-09-27 16:00:26 +0200

    deploy

---
+++ assets/DukqYcdM.js
@@ -0,0 +1,1 @@
+import{ap as n}from"./BClyMcC6.js";class s extends n{async show(e){if(typeof document<"u"){let a=2e3;e.duration&&(a=e.duration==="long"?3500:2e3);const t=document.createElement("pwa-toast");t.duration=a,t.message=e.text,document.body.appendChild(t)}}}export{s as ToastWeb};
